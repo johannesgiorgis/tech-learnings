66067f654b8a5c188273ffd472f73b87d8aef400
Johannes Giorgis <johannesgiorgis@users.noreply.github.com>
2019-05-15 14:06:13 -0700

    update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,8 +2,14 @@
 Calculator library containing basic math operations.
 """
 
+
 def add(first_term, second_term):
     return first_term + second_term
 
+
 def subtract(first_term, second_term):
     return first_term - second_term
+
+
+result = add(5, 4)
+print(f"5 + 4 = {result}")
