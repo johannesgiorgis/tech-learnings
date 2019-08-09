59bf372cb3feba0d72e476f6d1722aeacb88bf28
Johannes Giorgis <johannesgiorgis@users.noreply.github.com>
2019-08-08 20:35:42 -0700

    completed tutorial

--- flask-tutorial/tests/test_auth.py
+++ flask-tutorial/tests/test_auth.py
@@ -5,12 +5,13 @@ from flaskr.db import get_db
 
 def test_register(client, app):
     assert client.get("/auth/register").status_code == 200
-    response = client.post("/auth/resgister", data={"username": "a", "password": "a"})
+
+    response = client.post("/auth/register", data={"username": "a", "password": "a"})
     assert "http://localhost/auth/login" == response.headers["Location"]
 
     with app.app_context():
         assert (
-            get_db().execute("select * from user where username = 'a'").fetchone()
+            get_db().execute("SELECT * FROM user WHERE username = 'a'").fetchone()
             is not None
         )
 
