47ab8c9bd8dea2a045ce14d58144e21c01c356be
Johannes Giorgis <johannesgiorgis@users.noreply.github.com>
2019-09-02 21:06:05 -0700

    update chat app: - allow for user to pick port

--- goblueprints/chapter01/chat/main.go
+++ goblueprints/chapter01/chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -21,18 +22,20 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
-	fmt.Println("Starting web server on port 8080...")
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse() // parse the flags
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	// get the room going
 	go r.run()
 	// start the web server
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	fmt.Println("Starting web server on port", *addr, "...")
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
