5ca3e8021c0ea49479ac701ed09b2710509e503b
Johannes Giorgis <johannesgiorgis@users.noreply.github.com>
2020-02-24 23:10:23 -0800

    got login + logout working with conditional navigation headers to show different options based on user log in status

--- data-driven-web-apps-with-flask/pypi_org/views/account_views.py
+++ data-driven-web-apps-with-flask/pypi_org/views/account_views.py
@@ -1,6 +1,8 @@
 import flask
 
 from pypi_org.infrastructure.view_modifiers import response
+from pypi_org.services import user_service
+import pypi_org.infrastructure.cookie_auth as cookie_auth
 
 blueprint = flask.Blueprint("account", __name__, template_folder="templates")
 
@@ -10,7 +12,12 @@ blueprint = flask.Blueprint("account", __name__, template_folder="templates")
 @blueprint.route("/account")
 @response(template_file="account/index.html")
 def index():
-    return {}
+    user_id = cookie_auth.get_user_id_via_auth_cookie(flask.request)
+    user = user_service.find_user_by_id(user_id)
+    if not user:
+        return flask.redirect("/account/login")
+
+    return {"user": user}
 
 
 @blueprint.route("/<int:rank>")
@@ -32,23 +39,31 @@ def register_get():
 def register_post():
     r = flask.request
 
-    name = r.form.get('name')
-    email = r.form.get('email', '').lower().strip()
-    password = r.form.get('password', '').strip()
+    name = r.form.get("name")
+    email = r.form.get("email", "").lower().strip()
+    password = r.form.get("password", "").strip()
 
     if not name or not email or not password:
         return {
-            'name': name,
-            'email': email,
-            'password': password,
-            'error': "Some required fields are missing.",
+            "name": name,
+            "email": email,
+            "password": password,
+            "error": "Some required fields are missing.",
         }
 
-    # TODO: Create the user
-    # TODO: Log in browser as a session
+    user = user_service.create_user(name, email, password)
+    if not user:
+        return {
+            "name": name,
+            "email": email,
+            "password": password,
+            "error": "A user with that email already exists.",
+        }
 
-    print('Redirecting to account page :)')
-    return flask.redirect('/account')
+    resp = flask.redirect("/account")
+    cookie_auth.set_auth(resp, user.id)
+
+    return resp
 
 
 # ################### LOGIN #################################
@@ -63,7 +78,30 @@ def login_get():
 @blueprint.route("/account/login", methods=["POST"])
 @response(template_file="account/login.html")
 def login_post():
-    return {}
+    r = flask.request
+
+    email = r.form.get("email", "").lower().strip()
+    password = r.form.get("password", "").strip()
+
+    if not email or not password:
+        return {
+            "email": email,
+            "password": password,
+            "error": "Some required fields are missing.",
+        }
+
+    user = user_service.login_user(email, password)
+    if not user:
+        return {
+            "email": email,
+            "password": password,
+            "error": "The account does not exist or the password is incorrect.",
+        }
+
+    resp = flask.redirect("/account")
+    cookie_auth.set_auth(resp, user.id)
+
+    return resp
 
 
 # ################### LOGOUT #################################
@@ -71,4 +109,7 @@ def login_post():
 
 @blueprint.route("/account/logout")
 def logout():
-    return {}
+    resp = flask.redirect("/")
+    cookie_auth.logout(resp)
+
+    return resp
